7c53d67ae0d3a12f1f8f352ac7ebf50a94e59244
Julius Nedzinskas <juliusned@Juliuss-MBP.lan>
2020-02-07 22:18:28 -0500

    further script improvements

--- content_scripts/lightswitch.js
+++ content_scripts/lightswitch.js
@@ -1,40 +1,50 @@
+// const style = `
+// .lights {
+//   position: absolute;
+//   z-index: 999999;
+//   top: 5px;
+//   bottom: 5px;
+//   left: 5px;
+//   right: 5px;
+// }
+
+// .lights.on {
+//   border: 5px solid #fff300;
+//   box-shadow: inset 0px 0px 93px 79px rgba(255,243,0,0.76);
+// }
+
+// .lights.off {
+//   border: 5px solid #2e2e2e;
+//   box-shadow: inset 0x 0px 1px #fff;
+// }
+// `;
+
 (function() {
+
+  const styles = {
+    box: "position: fixed;z-index: 999999;top: 0px;bottom: 0px;left: 0px;right: 0px;pointer-events:none;",
+    on: "border: 5px solid #fff300;box-shadow: inset 0px 0px 93px 79px rgba(255,243,0,0.76);",
+    off: "border: 5px solid #2e2e2e;box-shadow: inset 0x 0px 6px 10px #fff;"
+  }
 
   if (window.hasRun) {
     return;
   }
   window.hasRun = true;
 
-  /**
-   * Given a URL to a beast image, remove all existing beasts, then
-   * create and style an IMG node pointing to
-   * that image, then insert the node into the document.
-   */
-  function insertBeast(beastURL) {
-    removeExistingBeasts();
-    let beastImage = document.createElement("img");
-    beastImage.setAttribute("src", beastURL);
-    beastImage.style.height = "100vh";
-    beastImage.className = "beastify-image";
-    document.body.appendChild(beastImage);
-  }
+  // determines whether it's daytime
+  let currentTime = new Date().getHours();
+  let day = (currentTime >= 8 && currentTime <= 18) ? true : false;
 
-  /**
-   * Remove every beast from the page.
-   */
-  function removeExistingBeasts() {
-    let existingBeasts = document.querySelectorAll(".beastify-image");
-    for (let beast of existingBeasts) {
-      beast.remove();
-    }
-  }
+  var light = document.createElement("div");
+  light.style = day ? styles.off + styles.box : styles.on + styles.box;
+  light.id = "light_switch";
+  document.body.appendChild(light);
 
   browser.runtime.onMessage.addListener((message) => {
-    if (message.command === "on") {
-      insertBeast(message.beastURL);
-    } else if (message.command === "off") {
-      removeExistingBeasts();
-    }
+    var lightNode = document.getElementById("light_switch");
+    if (message.command === "on") lightNode.style = styles.box + styles.on;
+    else if (message.command === "off") lightNode.style = styles.box + styles.off;
   });
 
 })();
