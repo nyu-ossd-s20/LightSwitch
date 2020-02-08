78c2e402f9ffa557a59b53bf1b5bcec2ea2f3d8c
Julius Nedzinskas <juliusned@Juliuss-MBP.lan>
2020-02-07 22:45:29 -0500

    added menu functionality

--- popup/menu.js
+++ popup/menu.js
@@ -1,9 +1,21 @@
-function clickListener() {
-
+function queryMessage(msg) {
+  browser.tabs.query({
+    currentWindow: true,
+    active: true
+  }).then(tabs => {
+    browser.tabs.sendMessage(tabs[0].id, {command: msg});
+  }).catch(alertError);
 }
 
 function alertError(error) {
   alert(`Failed to execute script: ${error.message}`);
+}
+
+function clickListener() {
+  document.addEventListener("click", (e) => {
+    x = e.target.id;
+    if (x === "on" || x === "off") queryMessage(x);
+  })
 }
 
 browser.tabs.executeScript({file: "/content_scripts/lightswitch.js"})
